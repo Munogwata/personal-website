df8228ec1e2b746586d77e7cf2062d6abbd7f59e
Munozovepi Gwata <muno.gwata@gmail.com>
2022-01-24 17:17:48 +0200

    added responsiveness to navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationMenuComponent } from './reuseable-components/navigation-menu/navigation-menu.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { LogoComponent } from './reusable-components/navigation/logo/logo.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { NavigationBarComponent } from './reuseable-components/navigation-bar/navigation-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationMenuComponent,
     LogoComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatButtonModule,
+    MatListModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
