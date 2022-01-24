09e28a30f64501c5254264857bd3fea3d6295f1d
Munozovepi Gwata <muno.gwata@gmail.com>
2022-01-24 02:42:09 +0200

    added the main menu, need to make it responsive and add the background as needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationMenuComponent } from './reuseable-components/navigation-menu/navigation-menu.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { LogoComponent } from './reusable-components/navigation/logo/logo.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationMenuComponent
+    NavigationMenuComponent,
+    LogoComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
